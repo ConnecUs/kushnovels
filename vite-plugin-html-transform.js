c7983a7b6436426de7b393c5f2f097e82dd481b5
ConnecUs <dazyners.mail@gmail.com>
2025-04-13 13:18:48 -0700

    Improve HTML transform plugin to move scripts from head to body

--- vite-plugin-html-transform.js
+++ vite-plugin-html-transform.js
@@ -3,11 +3,29 @@ export default function htmlTransform() {
   return {
     name: 'html-transform',
     transformIndexHtml(html) {
-      // Make sure gptengineer.js is loaded after the app script
-      return html.replace(
-        '</body>',
-        '<!-- IMPORTANT: DO NOT REMOVE THIS SCRIPT TAG OR THIS VERY COMMENT! -->\n    <script src="https://cdn.gpteng.co/gptengineer.js" type="module"></script>\n  </body>'
-      );
+      // Remove any existing gptengineer.js script
+      html = html.replace(/<script src="https:\/\/cdn\.gpteng\.co\/gptengineer\.js"[^>]*><\/script>/g, '');
+      
+      // Move all scripts from head to body
+      const headScripts = [];
+      html = html.replace(/<script([^>]*)>([\s\S]*?)<\/script>/g, (match, attrs, content) => {
+        if (html.indexOf(match) < html.indexOf('</head>')) {
+          headScripts.push({ attrs, content });
+          return '';
+        }
+        return match;
+      });
+      
+      // Insert all scripts at the end of body
+      return html.replace('</body>', () => {
+        let result = '';
+        headScripts.forEach(script => {
+          result += `<script${script.attrs}>${script.content}</script>\n    `;
+        });
+        result += '<!-- IMPORTANT: DO NOT REMOVE THIS SCRIPT TAG OR THIS VERY COMMENT! -->\n    ';
+        result += '<script src="https://cdn.gpteng.co/gptengineer.js" type="module"></script>\n  </body>';
+        return result;
+      });
     }
   };
 }
